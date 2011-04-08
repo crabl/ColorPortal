d7321b695aa1fecf3f9d8deea603a29324f49b0c
Christopher Rabl <christopher.rabl@uleth.ca>
2011-04-07 18:36:41 -0600

    Added Daltonization

--- viewportal.cpp
+++ viewportal.cpp
@@ -2,9 +2,8 @@
 
 Viewportal::Viewportal(QWidget *parent, int w, int h) : QLabel(parent) {
     setMouseTracking (true);
-
     setFixedSize(640,800);
-
+    setGeometry(QRect(640, 0, 1280, 800));
 
     screenX = QApplication::desktop() -> screenGeometry().width();
     screenY = QApplication::desktop() -> screenGeometry().height();
@@ -21,29 +20,11 @@ Viewportal::Viewportal(QWidget *parent, int w, int h) : QLabel(parent) {
         y = h;
     }
 
-    pixmap = QPixmap::grabWindow (QApplication::desktop()->winId(), 0, 0, x, y);
-    setPixmap (pixmap);
+    cbType = "d";
 
     QTimer *timer = new QTimer(this);
     connect(timer, SIGNAL(timeout()), this, SLOT(updatePixmap()));
-    timer->start(40);
-
-
-}
-
-void Viewportal::mouseMoveEvent(QMouseEvent *event) {
-   int left = event -> globalX() - x / 2;
-   if (left < 0)
-      left = 0;
-   else if (left + x > screenX)
-      left = screenX - x;
-   
-   int top = event -> globalY () - y / 2;
-   if (top < 0)
-      top = 0;
-   else if (top + y > screenY)
-      top = screenY - y;
-    updatePixmap();
+    timer->start(MAX_UPDATE_INTERVAL);
 }
 
 void Viewportal::resizeEvent(QResizeEvent *event) {
@@ -51,10 +32,34 @@ void Viewportal::resizeEvent(QResizeEvent *event) {
    y = height ();
 }
 
+QString Viewportal::getColorblindType() const { return cbType; }
+
+void Viewportal::setColorblindType(QString str) {
+    if(str == "d" || str == "t" || str == "p") {
+        cbType = str;
+    } else {
+        cbType = "d";
+    }
+}
+
 void Viewportal::updatePixmap() {
     pixmap = QPixmap::grabWindow (QApplication::desktop () -> winId (), 0, 51, 640, 800);
     image = pixmap.toImage ();
-    image.invertPixels();
-    pixmap = QPixmap::fromImage (image);
+
+    // image.invertPixels(); // Just to test
+
+    // PUT THIS INTO ANOTHER THREAD!!!
+    QString oldFile = QDir::currentPath()+"/image.jpg";
+    QString newFile = QDir::currentPath()+"/"+getColorblindType()+"-image.jpg";
+
+    image.save(oldFile);
+    std::string daltonizeCmd = ("python /home/crabl/Colorblind/daltonize.py "+getColorblindType()+" "+oldFile).toStdString();
+    std::string moveCmd = ("mv "+newFile+" "+oldFile).toStdString();
+
+    system(daltonizeCmd.c_str());
+    system(moveCmd.c_str());
+
+    QImage image2 = QImage(oldFile);
+    pixmap = QPixmap::fromImage (image2);
     setPixmap (pixmap);
 }
--- viewportal.h
+++ viewportal.h
@@ -1,29 +1,34 @@
 #ifndef VIEWPORTAL_H
 #define VIEWPORTAL_H
 
+#include <iostream>
 #include <QtGui>
 #include <QTimer>
 
-class Viewportal : public QLabel {
-   Q_OBJECT
-   
-  public:
-   Viewportal(QWidget *parent = 0, int w = 640, int h = 480);
+const int MAX_UPDATE_INTERVAL = 10000; // milliseconds
 
-  protected:
-   void mouseMoveEvent(QMouseEvent *event);
-   void resizeEvent(QResizeEvent *event);
+class Viewportal : public QLabel {
+    Q_OBJECT
+
+public:
+    Viewportal(QWidget *parent = 0, int w = 640, int h = 480);
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+    QString getColorblindType() const;
 
 public slots:
-   void updatePixmap();
+    void updatePixmap();
+    void setColorblindType(QString);
 
-  private:
-   int screenX;
-   int screenY;
-   int x;
-   int y;
-   QPixmap pixmap;
-   QImage image;
+private:
+    int screenX;
+    int screenY;
+    int x;
+    int y;
+    QPixmap pixmap;
+    QImage image;
+    QString cbType;
 };
 
 #endif
